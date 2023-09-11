5300e2c4523b3adedcaa200401a30bd6314a8494
bimalbp <find.bimal@gmail.com>
2023-09-10 17:19:42 -0700

    cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,37 +8,6 @@ export const metadata: Metadata = {
 		template: "%s | Bimal Paul",
 	},
 	description: "Engineering Manager at Capital One",
-	openGraph: {
-		title: "bimalpaul.dev",
-		description:
-			"Engineering Manager at Capital One",
-		url: "https://bimalpaul.dev",
-		siteName: "bimalpaul.dev",
-		images: [
-			{
-				url: "https://bimalpaul.dev/og.png",
-				width: 1920,
-				height: 1080,
-			},
-		],
-		locale: "en-US",
-		type: "website",
-	},
-	robots: {
-		index: true,
-		follow: true,
-		googleBot: {
-			index: true,
-			follow: true,
-			"max-video-preview": -1,
-			"max-image-preview": "large",
-			"max-snippet": -1,
-		},
-	},
-	twitter: {
-		title: "bimalpaul.dev",
-		card: "summary_large_image",
-	},
 	icons: {
 		shortcut: "/favicon.ico",
 	},
